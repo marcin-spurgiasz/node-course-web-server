9940b4e62381e84f679bc2d0dbc5c8f030c2811e
qxp4733 <marcin.spurgiasz@capgemini.com>
2018-07-22 14:27:30 +0200

    setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -21,9 +22,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -47,6 +48,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('server is up on 3000');
+app.listen(port, () => {
+    console.log(`server is up on ${port}`);
 });
